a34149bc5bc2e8b1d9e4909b8c4705a4883b019a
zhouzhengle <674899024@qq.com>
2023-06-23 13:17:28 +0800

    增加jprometheus-aom-huaweicloud-java-demo

---
+++ prometheus-aom-huaweicloud-java-demo/src/main/java/com/huaweicloud/aom/AppConfig.java
@@ -0,0 +1,9 @@
+package com.huaweicloud.aom;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    
+}
---
+++ prometheus-aom-huaweicloud-java-demo/src/test/java/com/huaweicloud/aom/HelloControllerTest.java
@@ -0,0 +1,20 @@
+package com.huaweicloud.aom;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.hamcrest.CoreMatchers.is;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class HelloControllerTest {
+
+
+    @Test
+    public void testHello() throws Exception {
+    }
+
+}
